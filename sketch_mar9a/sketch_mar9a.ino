96514a1d048769ade54b5eb24c834ee0bc34e3d4
Ravi Wallau <raviaw@gmail.com>
2024-03-11 00:27:01 -0300

    Update sketch_mar9a.ino

--- sketch_mar9a/sketch_mar9a.ino
+++ sketch_mar9a/sketch_mar9a.ino
@@ -1,37 +1,56 @@
 #include <AccelStepper.h>
 #include <elapsedMillis.h>
 
-AccelStepper verticalMotor(AccelStepper::DRIVER, 7, 6); 
-AccelStepper horizontalMotor(AccelStepper::DRIVER, 8, 9); 
+AccelStepper verticalMotor(AccelStepper::DRIVER, 4, 3); 
+AccelStepper horizontalMotor(AccelStepper::DRIVER, 6, 5); 
 
 elapsedMillis printTime;
+
+// Reduction X Axis = 1/ 198
+// Reduction Z Axis = 1/ 33
 
 void setup() {
     Serial.begin(115200);
   // put your setup code here, to run once:
-  verticalMotor.setMaxSpeed(1000.0);   // the motor accelerates to this speed exactly without overshoot. Try other values.
-  verticalMotor.setAcceleration(50.0);   // try other acceleration rates.
-  verticalMotor.moveTo(30000); 
+  verticalMotor.setMaxSpeed(2000.0);   // the motor accelerates to this speed exactly without overshoot. Try other values.
+  verticalMotor.setAcceleration(100.0);   // try other acceleration rates.
+  verticalMotor.moveTo(79200); 
+  horizontalMotor.setMaxSpeed(100.0);   // the motor accelerates to this speed exactly without overshoot. Try other values.
+  horizontalMotor.setAcceleration(50.0);   // try other acceleration rates.
+  horizontalMotor.moveTo(-5000); 
 }
 
 float stepsPerDegree = 1000;
 
 int count = 0;    // tracks seconds to trigger an action if desired.
 void loop() {
-  float mSpeed;
+  float verticalSpeed;
+  float horizontalSpeed;
   if (printTime >= 1000) {    // reports speed and position each second
     printTime = 0;
-    mSpeed = verticalMotor.speed();
-    Serial.print(mSpeed);
+    verticalSpeed = verticalMotor.speed();
+    Serial.print("V: ");
+    Serial.print(verticalSpeed);
     Serial.print("  ");
     Serial.print(verticalMotor.currentPosition());
     Serial.print("  ");
     Serial.println(count);
-    if (count++ == 30){      // uncomment an action to see the response in the serial monitor
-      verticalMotor.moveTo(100);
+
+    horizontalSpeed = horizontalMotor.speed();
+    Serial.print("H: ");
+    Serial.print(horizontalSpeed);
+    Serial.print("  ");
+    Serial.print(horizontalMotor.currentPosition());
+    Serial.print("  ");
+    Serial.println(count);
+
+    if (count++ == 75){      // uncomment an action to see the response in the serial monitor
+      verticalMotor.moveTo(0);
+      horizontalMotor.moveTo(0);
       //myStepper.move(100);
       //myStepper.stop();
     }
   }
   verticalMotor.run();
+  horizontalMotor.run();
 }
